8c752b91af8eb673b0cb09295b518bcf21bbd259
Alessandro <alessandro.porcheddu@studio.unibo.it>
2024-11-26 15:46:42 +0100

    changing dir

--- 101-lambda-utilities-for-lists-and-maps/src/main/java/it/unibo/oop/lab/lambda/LambdaUtilities.java
+++ 101-lambda-utilities-for-lists-and-maps/src/main/java/it/unibo/oop/lab/lambda/LambdaUtilities.java
@@ -2,6 +2,8 @@ package it.unibo.oop.lab.lambda;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -58,10 +60,12 @@ public final class LambdaUtilities {
      *         otherwise.
      */
     public static <T> List<Optional<T>> optFilter(final List<T> list, final Predicate<T> pre) {
-        /*
-         * Suggestion: consider Optional.filter
-         */
-        return null;
+        final List<Optional<T>> retList = new LinkedList<>();
+        list.forEach(elem -> {
+            final Optional<T> toAdd = elem == null ? Optional.empty() : Optional.of(elem);
+            retList.add(toAdd.filter(pre));
+        });
+        return retList;
     }
 
     /**
@@ -77,10 +81,14 @@ public final class LambdaUtilities {
      *         based on the mapping done by the function
      */
     public static <R, T> Map<R, Set<T>> group(final List<T> list, final Function<T, R> op) {
-        /*
-         * Suggestion: consider Map.merge
-         */
-        return null;
+        final Map<R, Set<T>> retMap = new HashMap<>();
+        list.forEach(elem -> {
+            retMap.merge(op.apply(elem), new HashSet<>(), (elem1, elem2) -> {
+                elem1.addAll(elem2);
+                return elem1;
+            });
+        });
+        return retMap;
     }
 
     /**
@@ -100,8 +108,12 @@ public final class LambdaUtilities {
          * Suggestion: consider Optional.orElse
          *
          * Keep in mind that a map can be iterated through its forEach method
-         */
-        return null;
+        */
+        final Map<K, V> retMap = new HashMap<>();
+        map.forEach((key, value) -> {
+            retMap.put(key, value.orElse(def.get()));
+        });
+        return retMap;
     }
 
     /**
